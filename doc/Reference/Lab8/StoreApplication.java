ecfa2ef7c6c69362fc6d43d63cb2910fa57071e7
97116368 <97116368@e1-b1401-15.ad.feit.uts.edu.au>
2016-05-24 15:15:00 +1000

    house keeping

---
+++ doc/Reference/Lab8/StoreApplication.java
@@ -0,0 +1,21 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.geometry.*;
+import javafx.fxml.*;
+
+public class StoreApplication extends Application {
+    public static void main(String[] args) { launch(args); }
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("store.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Store");
+        stage.setScene(new Scene(root));
+        stage.sizeToScene();
+        stage.show();
+    }
+}
