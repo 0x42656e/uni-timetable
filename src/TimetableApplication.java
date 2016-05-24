8c2e300f583a8b3d07ff3e2c8840d9d7dcecc8cb
97116368 <97116368@e1-b1401-15.ad.feit.uts.edu.au>
2016-05-24 14:57:36 +1000

    initial commit

---
+++ src/TimetableApplication.java
@@ -0,0 +1,12 @@
+import au.edu.uts.ap.javafx.*;
+import model.*;
+import javafx.application.*;
+import javafx.stage.*;
+
+public class TimetableApplication extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
