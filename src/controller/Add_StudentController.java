cba681df89a76c120671449f5942411407d5adba
EpicPC <epicpc@EpicPC.Home>
2016-05-27 01:01:37 +1000

    add_student rules

--- src/controller/Add_StudentController.java
+++ src/controller/Add_StudentController.java
@@ -1,13 +1,35 @@
 package controller;
 
 import au.edu.uts.ap.javafx.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import model.*;
+import javafx.beans.binding.*;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.*;
 
-import javax.sound.sampled.Control;
-
-/**
- * Created by epicpc on 5/26/16.
- */
 public class Add_StudentController extends Controller<Student> {
 
+    @FXML TextField numberTf;
+    @FXML TextField nameTf;
+    @FXML Button addBtn;
+    @FXML ToggleGroup attendanceTg;
+    @FXML CheckBox scholarshipCb;
+    boolean ben = true;
+
+    @FXML
+    private void initialize() {
+         BooleanBinding booleanBind = Bindings.or(numberTf.textProperty().isEmpty(),
+         nameTf.textProperty().isEmpty());
+
+        addBtn.disableProperty().bind(attendanceTg.selectedToggleProperty().isNull() .or(booleanBind));
+
+    }
+
+    @FXML
+    public void handleCancel() throws Exception {
+
+        stage.close();
+    }
 }
