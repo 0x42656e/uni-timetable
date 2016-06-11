229e97df1cd49dd9760b8f69d2aff31b3a26a74d
B. F <benjamin@benjamins-mbp.home>
2016-06-11 10:28:06 +1000

    changes to make public

--- doc/Reference/Extras/week9/controls/ControlsController.java
+++
@@ -1,6 +0,0 @@
-import au.edu.uts.ap.javafx.*;
-import model.*;
-
-public class ControlsController extends Controller<Customer> {
-	public final Customer getCustomer() { return model; }
-}
--- doc/Reference/mvc-self-demo/model/Sobek.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Sobek {
-    public Sobek() {
-        System.out.println("Sobek initialised");
-    }
-}
